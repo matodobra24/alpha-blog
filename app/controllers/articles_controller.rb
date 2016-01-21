be4f48b9863432080e392cdf126ab0315a0ccaa9
Johnathan Matodobra <matodobra24@gmail.com>
2016-01-21 01:05:44 +0000

    Added partials & destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,15 @@
 class ArticlesController < ApplicationController
     
+    def index
+        @articles = Article.all
+    end
     #With in new action page. @article is equal to createing a new Article Database Object. Saves it as @article.
     def new
         @article = Article.new
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
     end
     
     def create
@@ -15,10 +22,26 @@ class ArticlesController < ApplicationController
         end
     end
     
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article Was Successfully Updated!"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+    
     def show
         @article = Article.find(params[:id])
     end
     
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "You have successfully deleted this article!"
+        redirect_to articles_path
+    end
     private 
         def article_params
             params.require(:article).permit(:title, :description)
