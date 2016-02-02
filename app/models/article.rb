50f0c31691a21b6ae794a4f667b08b8d240c1d22
Johnathan Matodobra <matodobra24@gmail.com>
2016-02-02 04:08:42 +0000

    Added association between user and articles and added debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
+    belongs_to :user
     #Create Validations that prevent nil elements. Forces Constraints!
     validates :title, presence: true, length: { minimum: 3, maximum: 50 }
     validates :description, presence: true, length: { minimum: 10, maximum: 300 }
-    
+    validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
+    
+    has_many :articles
+    
+    #before user email is saved it saves it as downcase
+    before_save { self.email = email.downcase }
     #Validation tools. Validates :username presence to show username isnt blank
     #Uniqueness ensures that usernames arent the same and are not case sensitive
     #Usernames have a minimum length of 3 characters & a max of 25
     #Lecture #93
+    
     validates :username, presence: true, 
                             uniqueness: { case_sensitive: false }, 
                             length: { minimum: 3, maximum: 25}
