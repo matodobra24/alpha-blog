d0d2850611483839e6cb0f9e79c555b4ec0267a7
Johnathan Matodobra <matodobra24@gmail.com>
2016-02-02 00:50:10 +0000

    Added Validations to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
+    #Validation tools. Validates :username presence to show username isnt blank
+    #Uniqueness ensures that usernames arent the same and are not case sensitive
+    #Usernames have a minimum length of 3 characters & a max of 25
+    #Lecture #93
+    validates :username, presence: true, 
+                            uniqueness: { case_sensitive: false }, 
+                            length: { minimum: 3, maximum: 25}
+                            
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     
+    validates :email, presence: true, uniqueness: {case_sensitive: false},
+                            format: { with: VALID_EMAIL_REGEX },
+                            length: { minimum: 3, maximimum: 25 }
 end
